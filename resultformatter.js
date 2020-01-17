fdbd5e80907d00bdf4ea882d9c98646164ad8b31
hehaijin <hehaijin1981@gmail.com>
2020-01-16 21:16:45 -0700

    clean up:

--- resultformatter.js
+++ resultformatter.js
@@ -1,31 +1,9 @@
-function test() {
-	let nodes = document.querySelectorAll('.r > a');
-	console.log('nodes length', nodes.length);
-	nodes.forEach(ele => {
-		let url = ele.childNodes[0];
-		let title = ele.childNodes[2];
-		ele.insertBefore(title, url);
-		url.childNodes[1].textContent = url.childNodes[1].textContent + '\xA0\xA0\xA0\xA0\xA0\xA0';
-
-		url.style.position = 'relative';
-		let parent = ele.parentNode;
-		let cache = parent.childNodes[1];
-		parent.removeChild(cache);
-		url.appendChild(cache);
-
-	});
-
-}
-
-// var target = document.querySelector('.r > a');
 var config = { subtree: true, childList: true }
 
 const observer = new MutationObserver((mutations) => {
-
 	mutations.forEach(
 		m => {
 			if (m.type === 'childList' && m.target.tagName === 'DIV' && m.target.className === 'r') {
-
 				m.addedNodes.forEach(ele => {
 					let url = ele.childNodes[0];
 					let br= ele.childNodes[1];
@@ -33,7 +11,6 @@ const observer = new MutationObserver((mutations) => {
 					if (url && title) {
 						ele.removeChild(br);
 						ele.insertBefore(title, url);
-						//url.childNodes[1].textContent = url.childNodes[1].textContent + '\xA0\xA0\xA0\xA0\xA0\xA0';
 						title.style.display='block';
 						url.style.display='block';						
 						url.style.position = 'relative';
@@ -42,13 +19,10 @@ const observer = new MutationObserver((mutations) => {
 						parent.removeChild(cache);
 						url.appendChild(cache);
 					}
-					
-
 				})
 
 			}
 		}
-
 	);
 
 }
