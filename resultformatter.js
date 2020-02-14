b7afe79dae7ef90ced30a9445594ee75f986607b
hehaijin <hehaijin1981@gmail.com>
2020-02-13 21:15:49 -0700

    update to fix recent google search format;using querySelector instead of index to make it more robust

--- resultformatter.js
+++ resultformatter.js
@@ -3,47 +3,16 @@ const config = { subtree: true, childList: true }
 const observer = new MutationObserver((mutations) => {
 	mutations.forEach(
 		m => {
-			if(m.target.tagName === 'A'){
-				m.addedNodes.forEach(ele=>{
-					console.log(ele);
-				
-
-
-				})
-
-			}
-
 			if (m.type === 'childList' && m.target.tagName === 'DIV' && m.target.className === 'r') {
 				m.addedNodes.forEach(ele => {
-					// old format
-					// console.log('old format');
-					if (ele.childNodes.length === 3) {
-						let url = ele.childNodes[0];
-						let br = ele.childNodes[1];
-						let title = ele.childNodes[2];
-						if (url && title) {
-							ele.removeChild(br);
-							ele.insertBefore(title, url);
-							title.style.display = 'block';
-							url.style.display = 'block';
-							url.style.position = 'relative';
-							let parent = ele.parentNode;
-							let cache = parent.childNodes[1];
-							parent.removeChild(cache);
-							url.appendChild(cache);
-						}
-					} else {
-					// a new format
-						console.log('child length 2');
-						console.log(ele.childNodes[0].childNodes.length);
-						if(ele.querySelector('h3')){
-					  
-							console.log('plugin working');
-						
-						}
-
+					if (ele.tagName === 'A') {
+						ele.querySelector('br').remove();
+						let url = ele.querySelector('div.TbwUpd.NJjxre');
+						url.style.display = 'block';
+						url.style.position = 'relative';
+						let cache = ele.parentNode.querySelector('div.B6fmyf');
+						url.appendChild(cache);
 					}
-
 				})
 
 			}
