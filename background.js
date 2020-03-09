24612fe0cdbc446cf3e3637ecb013db493788bb8
hehaijin <hehaijin1981@gmail.com>
2020-03-09 15:20:31 -0600

    pagestatematcher

--- background.js
+++ background.js
@@ -21,7 +21,7 @@ chrome.runtime.onInstalled.addListener(function () {
     chrome.declarativeContent.onPageChanged.removeRules(undefined, function () {
         chrome.declarativeContent.onPageChanged.addRules([{
             conditions: [new chrome.declarativeContent.PageStateMatcher({
-                pageUrl: { hostContains: 'google' },
+                pageUrl: { urlMatches: 'google.*search' },
             })
             ],
             actions: [new chrome.declarativeContent.ShowPageAction()]
