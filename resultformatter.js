2148ae76cd72732f59e28f20ce9534a748c75926
hehaijin <hehaijin1981@gmail.com>
2020-10-05 20:39:05 -0600

    update with google recent change

--- resultformatter.js
+++ resultformatter.js
@@ -3,8 +3,10 @@ const config = { subtree: true, childList: true }
 const observer = new MutationObserver((mutations) => {
 	mutations.forEach(
 		m => {
-			if (m.type === 'childList' && m.target.tagName === 'DIV' && m.target.className === 'r') {
+			// for each seach result, which is a div and with class r, this 
+			if (m.type === 'childList' && m.target.tagName === 'DIV' && m.target.className === 'yuRUbf') {
 				m.addedNodes.forEach(ele => {
+					// the title + website component is a anchor 
 					if (ele.tagName === 'A') {
 						ele.querySelector('br').remove();
 						let url = ele.querySelector('div.TbwUpd.NJjxre');
